52d041be459afac190083a21ba95f804a046bb67
A1anzzz <61769780+A1anzzz@users.noreply.github.com>
2022-05-14 18:30:58 +0800

    Update api.js

--- weather-app/routes/api.js
+++ weather-app/routes/api.js
@@ -1,3 +1,9 @@
+//CHEN Zixuan 1155124576 (ESTR2106)
+//LI Yanxun 1155124374
+//LU Haoyu 1155124277
+//XIAO Zeyu 1155124570
+//ZHANG Yinan 1155124345
+
 const express = require('express');
 const router = express();
 const Location = require('../models/location');
